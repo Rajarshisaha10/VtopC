44fbf33a348b41bd90a684b8402334dd6daa1471
Vedant <133109553+VedantJJA@users.noreply.github.com>
2025-12-24 21:12:51 +0530

    Update greeting from 'Hello World' to 'Goodbye World'

--- static/dashboard.js
+++ static/dashboard.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // 2. Force Reload/Login
             window.location.href = '/login';
         }
-    }, 1000); // 5 Seconds Timeout
+    }, 1500); // 5 Seconds Timeout
     // ============================================================
 
     // State for secure directory
@@ -636,6 +636,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
 
 
 
