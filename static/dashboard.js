7a40c6af847624185ca7fa53de3db17a602efb4f
Vedant <133109553+VedantJJA@users.noreply.github.com>
2025-12-24 16:25:58 +0530

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // 2. Force Reload/Login
             window.location.href = '/login';
         }
-    }, 1000); // 5 Seconds Timeout
+    }, 100); // 5 Seconds Timeout
     // ============================================================
 
     // State for secure directory
@@ -636,3 +636,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
